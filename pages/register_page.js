5efef48a22459048702d6d208875a11108632379
George <george@listinglogic.com>
2023-11-17 09:45:45 +1300

    tidy up the code

--- pages/register_page.js
+++ pages/register_page.js
@@ -1,5 +1,6 @@
 export class registerPage{
 
+    //Element locators
     weblocators = {
         firstName:'#input-firstname',
         lastName:'#input-lastname',
@@ -11,43 +12,43 @@ export class registerPage{
         continueBtn: '.btn.btn-primary',
     }
 
+    //Open register page
     openRegisterPage() {
-
         cy.visit(Cypress.env('registerURL'))
     }
 
+    //Enter first name
     enterFirstName(FName) {
-
         cy.get(this.weblocators.firstName).type(FName)
     }
 
+    //Enter last name
     enterlastName(LName) {
-
         cy.get(this.weblocators.lastName).type(LName)
     }
 
+    //Enter email
     enterEmail(email) {
-
         cy.get(this.weblocators.email).type(email)
     }
 
+    //Enter phone number
     enterTelephone(phoneNo) {
-
         cy.get(this.weblocators.telephone).type(phoneNo)
     }
 
+    //Enter password
     enterPassword(password) {
-
         cy.get(this.weblocators.password).type(password)
         cy.get(this.weblocators.passwordConfirm).type(password)
-
     }
 
+    //Tick the checkbox
     selectCheckbox() {
-
         cy.get(this.weblocators.policyCheckbox).check()
     }
 
+    //Cilck continue button to submit register
     clickOnContinue() {
         cy.get(this.weblocators.continueBtn).click()
     }       
